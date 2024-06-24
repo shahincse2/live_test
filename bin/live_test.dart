8504a96ac14269af7773148e8ac8aebdb106cee0
shahincse2 <shahinalam.cse2@gmail.com>
2024-06-24 23:18:15 +0600

    Initial commit

---
+++ bin/live_test.dart
@@ -0,0 +1,25 @@
+class Media {
+  void play() {
+    print("Playing media...");
+  }
+}
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
+
+void main() {
+  Media media = Media();
+
+  Song song = Song("John Doe");
+
+  media.play();
+  song.play();
+}
